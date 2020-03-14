31c97f284e6c0a51c68997da69930bc1507d171d
alexv <alexv@DESKTOP-MJE32B0>
2020-03-14 18:41:47 +0100

    Hola GitHub!

---
+++ src/entornospractica/HolaGitHub.java
@@ -0,0 +1,10 @@
+package entornospractica;
+
+public class HolaGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola GitHub!");
+	}
+
+}
